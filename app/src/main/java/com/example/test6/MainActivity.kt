ee537224a11efb97657ab6e3e0a4024b94b0216a
Roberto Frabetti <roberto.frabetti@studio.unibo.it>
2022-05-16 18:35:37 +0100

    third

--- app/src/main/java/com/example/test6/MainActivity.kt
+++ app/src/main/java/com/example/test6/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            saa
         }
     }
 
